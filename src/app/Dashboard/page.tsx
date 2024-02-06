4a65ed9fd768335b123f1fc4b06fe23e75877464
Gabrielle <gabrielle.castro@ymonetize.com>
2024-02-06 18:30:53 -0300

    at1 dashboard

--- src/app/Dashboard/page.tsx
+++ src/app/Dashboard/page.tsx
@@ -1,9 +1,40 @@
-import styles from './page.module.scss'
+import styles from './page.module.scss';
+import { SiSimpleanalytics } from "react-icons/si";
+import Header from '../../components/Header/page'
 
 export default function Dashboard() {
+  const arrayInfoEarnings = [
+    { backgroundColor: '#F18A0E', period: 'Hoje (até o momento)', value: '1.238,00' },
+    { backgroundColor: '#F1370E', period: 'Ontem', value: '2.248,00' },
+    { backgroundColor: '#3BF10E', period: 'Últimos 7 dias', value: '19.124,00' },
+    { backgroundColor: '#2E0EF1', period: 'Este mês', value: '29.124,00' },
+    { backgroundColor: '#A80EF1', period: 'Próximo pagamento', value: '120.238,00' },
+  ]
+
   return (
     <div className={styles.container}>
+      <Header />
+      <div className={styles.content}>
+        <h1 className={styles.titleHeader}>Dashboard</h1>
 
+        <div className={styles.box}>
+          <h2>Ganhos estimados</h2>
+
+          <div className={styles.contentEarnings}>
+            {arrayInfoEarnings.map(item => (
+              <div className={styles.cardEarnings}>
+                <div style={{ backgroundColor: item.backgroundColor }} className={styles.contentIcon}>
+                  <SiSimpleanalytics className={styles.icon} />
+                </div>
+                <div className={styles.contentInfoEarnings}>
+                  <label>{item.period}</label>
+                  <h3>R${item.value}</h3>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/Header/page.tsx
+++ src/components/Header/page.tsx
@@ -1,9 +1,48 @@
-import styles from './page.module.scss'
+import Image from 'next/image';
+import styles from './page.module.scss';
+import { MdSpaceDashboard } from "react-icons/md";
+import { IoDocumentText } from "react-icons/io5";
+import { FaUser, FaMoneyCheck } from "react-icons/fa";
+import { IoIosExit } from "react-icons/io";
 
 export default function Header() {
   return (
     <div className={styles.container}>
+      <div>
+        <Image
+          src="/ymonetize.png"
+          width={180}
+          height={70}
+          alt="ymbg"
+        />
 
+        <div className={styles.content}>
+          <div className={styles.box}>
+            <MdSpaceDashboard size={20} className={styles.icon} />
+            <h1>Dashboard</h1>
+          </div>
+
+          <div className={styles.box}>
+            <IoDocumentText size={20} className={styles.icon} />
+            <h1>Relatorios</h1>
+          </div>
+
+          <div className={styles.box}>
+            <FaMoneyCheck size={20} className={styles.icon} />
+            <h1>Pagamentos</h1>
+          </div>
+
+          <div className={styles.box}>
+            <FaUser size={20} className={styles.icon} />
+            <h1>Perfil</h1>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.boxExit}>
+        <IoIosExit size={30} className={styles.icon} />
+        <h1>Sair</h1>
+      </div>
     </div>
   )
 }
